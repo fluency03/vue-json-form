3e56bb0e26e3ab226bc745594daaa8a5b10aab33
fluency03 <fluency.03@gmail.com>
2017-04-06 13:16:45 +0200

    json form

---
+++ src/JsonForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Form class="json-form" :label-width="labelWidth">
+      <json-node :prop="name" :value="data"></json-node>
+    </Form>
+  </div>
+</template>
+
+<script>
+  import JsonNode from './JsonNode'
+
+  export default {
+    components: {
+      JsonNode
+    },
+    props: {
+      labelWidth: {
+        type: Number,
+        default: 40
+      },
+      name: {
+        type: String,
+        default: ''
+      },
+      data: {
+        type: Object
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
